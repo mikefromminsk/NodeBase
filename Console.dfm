5c21dad43914fa3cad40093a0dbab54c8ab3fb6e
x29a100 <x29a100@b0f4f0ed-4edc-434a-b90a-db1df2d90469>
2013-09-17 13:23:36 +0000

    git-svn-id: svn://100.93.93.31/meta@39 b0f4f0ed-4edc-434a-b90a-db1df2d90469

--- Console.dfm
+++ Console.dfm
@@ -10,6 +10,37 @@ object GG: TGG
   Font.Name = 'MS Sans Serif'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
+  object Splitter1: TSplitter
+    Left = 241
+    Top = 0
+    Height = 300
+  end
+  object Console: TRichEdit
+    Left = 0
+    Top = 0
+    Width = 241
+    Height = 300
+    Align = alLeft
+    BevelInner = bvNone
+    BevelOuter = bvNone
+    BorderStyle = bsNone
+    TabOrder = 0
+  end
+  object InputData: TRichEdit
+    Left = 244
+    Top = 0
+    Width = 202
+    Height = 300
+    Align = alClient
+    BevelInner = bvNone
+    BevelOuter = bvNone
+    BorderStyle = bsNone
+    Lines.Strings = (
+      'Hello'
+      'world')
+    TabOrder = 1
+  end
 end
--- Console.pas
+++ Console.pas
@@ -4,10 +4,14 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, StdCtrls, ComCtrls, ExtCtrls;
 
 type
   TGG = class(TForm)
+    Console: TRichEdit;
+    InputData: TRichEdit;
+    Splitter1: TSplitter;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +24,32 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure Add(Str: String);
+begin
+  GG.Console.Lines.Add(Str);
+end;
+
+procedure TGG.FormCreate(Sender: TObject);
+var
+  i: Integer;
+  nvar, ndata: Integer;
+begin
+  Randomize;
+
+
+  ndata := InputData.Lines.Count;
+  for i:=0 to ndata - 1 do
+    Add('!' + InputData.Lines[i] + #10);
+
+  Add(IntToSTr(InputData.Lines.Count) + #10);
+  for i:=0 to ndata - 1 do
+    Add(IntToSTr(Length(InputData.Lines[i])) + #10);
+
+  nvar := Random(5);
+  for i:=0 to nvar do
+    Add('var' + IntToStr(i) + #10);
+
+end;
 
 end.
