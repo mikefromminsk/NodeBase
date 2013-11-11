a34306fcfbfd9babc0fbf8f8bc9c4c347993782e
x29a100 <x29a100@b0f4f0ed-4edc-434a-b90a-db1df2d90469>
2013-11-11 14:18:12 +0000

    git-svn-id: svn://100.93.93.31/meta@167 b0f4f0ed-4edc-434a-b90a-db1df2d90469

--- MetaBase.pas
+++ MetaBase.pas
@@ -3,8 +3,8 @@ unit MetaBase;
 interface
 
 uses
-  SysUtils{AllocMem, Now}, Classes{TStrings}, MetaLine, Windows{SetTimer},
-  MetaUtils, Dialogs;
+  Types{TIntegerDynArray},  Math{SumInt}, SysUtils{AllocMem, Now},
+  Classes{TStrings}, MetaLine, Windows{SetTimer}, MetaUtils, Dialogs;
 
 type
   TimerProc = procedure (wnd: HWND; uMsg, idEvent: UINT; dwTime: DWORD) of object; stdcall;
@@ -836,6 +836,75 @@ begin
   Dispose(TimeLine);
 end;
 
+
+
+
+
+var
+  ValueActionFrequency: TIntegerDynArray;
+
+const
+  actNew = 0;
+  actFind = 1;
+  actDelete = 2;
+
+
+
+function RandomArr(A: TIntegerDynArray): Integer;
+var i: Integer;
+begin
+  Result := Random(MaxInt);
+  Result := Result mod SumInt(A);
+  for i:=0 to High(A) do
+  begin
+    Result := Result - A[i];
+    if Result <= 0 then
+    begin
+      Result := i;
+      Exit;
+    end;
+  end;
+end;
+
+
+function RandomNode(Node: PNode): PNode;
+begin
+  Node := Node.ParentLocal;
+  Result := Node.Local[Random(High(Node.Local) + 1)];
+end;
+
+
+procedure TypeLink(Node: PNode; Value, Next, Fif: Integer);
+var
+  TypeLink, Action: Integer;
+begin
+  TypeLink := Value;
+
+  if TypeLink <= Value then
+  begin
+    case RandomArr(ValueActionFrequency) of
+      actNew : Node.Value := Base.NewNode(Base.NextID);   //Analusis
+      actFind: Node.Value := Base.NewNode(Base.GetIndex(RandomNode(Node)));  //Recursion  Param
+      actDelete: Base.SaveNode(Base.AddLocal(Node, Node.Value)); //ParentValue
+    end;
+  end;
+
+end;
+
+
+procedure Analysis(Node: PNode);
+begin
+  if Node = nil then Exit;
+  if Node.Interest > 0 then
+  begin
+    if Node.Prev = nil then
+      TypeLink(Node, 10, 10, 10);
+  end;
+end;
+
+
+
+
 function TMeta.Get(Line: String): PNode;
 var Data: String;
 begin
@@ -852,6 +921,12 @@ begin
   end;
 end;
 
-//initialization
+
+initialization
   //Base := TMeta.Create;
+  Randomize;
+  SetLength(ValueActionFrequency, 3);
+  ValueActionFrequency[actNew] := 0;
+  ValueActionFrequency[actFind] := 90;
+  ValueActionFrequency[actDelete] := 0;
 end.
