56ec434c2b7824403f3bced4ee21cd5f13b4d61a
x29a100 <x29a100@b0f4f0ed-4edc-434a-b90a-db1df2d90469>
2014-02-05 09:05:21 +0000

    git-svn-id: svn://100.93.93.31/meta@268 b0f4f0ed-4edc-434a-b90a-db1df2d90469

---
+++ src/Notepad/src/com/example/notepad/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.notepad;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+		ListView List = (ListView)findViewById(R.id.listView1);
+		String[] arr = {"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"};
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, arr);
+		List.setAdapter(adapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
