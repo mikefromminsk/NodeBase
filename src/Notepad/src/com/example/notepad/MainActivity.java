53ef299d55b173c3cdd5e6e1d83f88eac209c466
x29a100 <x29a100@b0f4f0ed-4edc-434a-b90a-db1df2d90469>
2014-02-07 07:40:45 +0000

    git-svn-id: svn://100.93.93.31/meta@271 b0f4f0ed-4edc-434a-b90a-db1df2d90469

--- src/Notepad/src/com/example/notepad/MainActivity.java
+++ src/Notepad/src/com/example/notepad/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.notepad;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -22,6 +35,27 @@ public class MainActivity extends Activity {
 		String[] arr = {"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"};
 		MetaAdapter adapter = new MetaAdapter(this, R.layout.list_item);
 		List.setAdapter(adapter);
+		
+		Button button = (Button)findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0){
+				try
+				{
+				    HttpClient client = new DefaultHttpClient();  
+				    HttpGet get = new HttpGet("http://178.124.178.151/1/test.txt");
+				    HttpResponse responseGet = client.execute(get);  
+				    HttpEntity resEntityGet = responseGet.getEntity();  
+				    if (resEntityGet != null) 
+				    {  
+				        String response = EntityUtils.toString(resEntityGet);
+				        Toast.makeText(getApplication(), "Запрос выполнен", Toast.LENGTH_SHORT).show(); 
+				    }
+				}catch (Exception e)
+				{Log.i("GET RESPONSE", "Error " + e.getMessage());}
+			}
+		});
 	}
 
 	@Override
--- src/Notepad/src/com/example/notepad/MetaAdapter.java
+++ src/Notepad/src/com/example/notepad/MetaAdapter.java
@@ -9,12 +9,26 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MetaAdapter extends ArrayAdapter {
@@ -27,46 +41,62 @@ public class MetaAdapter extends ArrayAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		return super.getView(position, convertView, parent);
+		LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View row = inflater.inflate(R.layout.list_item, parent, false);
+		
+		TextView textView = (TextView)row.findViewById(R.id.textView1);
+		textView.setTag("http://178.124.178.151/1/test.txt");
+		new DownloadNode().execute(textView);
+		return row;
 	}
+
+	@Override
+	public int getCount() {
+		return 30;
+	}
+	
+	
+	class DownloadNode extends AsyncTask<Object, Void, String>{
+
+		TextView textView;
+
+        @Override
+        protected String doInBackground(Object... params) {
+        	textView = (TextView)params[0];
+        	try
+        	{
+				URLConnection conn = new URL(textView.getTag().toString()).openConnection();
+				BufferedReader buf = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				return buf.readLine();
+    		}catch (Exception e)
+    		{Log.i("GET RESPONSE", "Error " + e.getMessage());}
+        	
+            
+			return null;
+        }
+        
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			textView.setText(result);
+		}
+    }
+
 
 	final String FileName = "file";
 	  
-	  void writeFile() {
-		  
-	
-			try 
-			{
+	  void writeFile() throws IOException {
 				FileWriter file = new FileWriter(FileName);
 				file.write("123"); 
 				file.close();
-				
-			} catch (IOException e) 
-			{
-				e.printStackTrace();
-			}  
-
-		 
 		  }
 
-		  void readFile() 
-		  {
-		    try 
-		    {
+		  void readFile() throws IOException {
 		    	String str = "";
 		    	BufferedReader buf = new BufferedReader(new FileReader(FileName));
 		    	while ((str = buf.readLine()) != null) 
-		    	{
 			        Log.d("Adapter", str); 
-		    	}
 		    	buf.close();
-			} 
-		    catch (IOException e) 
-		    {
-			      e.printStackTrace();
-			}
-			    
-		      
 		  }
 
 }
