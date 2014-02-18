6c81dce70d533fe19455a732cf9856f22af14539
x29a100 <x29a100@b0f4f0ed-4edc-434a-b90a-db1df2d90469>
2014-02-18 10:56:07 +0000

    git-svn-id: svn://100.93.93.31/meta@301 b0f4f0ed-4edc-434a-b90a-db1df2d90469

--- src/Notepad/gen/com/example/notepad/R.java
+++ src/Notepad/gen/com/example/notepad/R.java
@@ -22,12 +22,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int cycle=0x7f020000;
+        public static final int shadowed_background=0x7f020001;
+        public static final int tv_background_with_divider=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080005;
         public static final int button1=0x7f080002;
         public static final int editText1=0x7f080001;
-        public static final int listView1=0x7f080003;
+        public static final int listView1=0x7f080004;
+        public static final int listViewBackground=0x7f080003;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
--- src/Notepad/src/com/example/notepad/List.java
+++ src/Notepad/src/com/example/notepad/List.java
@@ -10,6 +10,7 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
@@ -19,11 +20,14 @@ import android.widget.Toast;
 public class List extends ArrayAdapter {
 	
 	Context context;
+	View.OnTouchListener mTouchListener;
 	Node root;
+	
 
-	public List(Context context, int resource, Node root) {
+	public List(Context context, int resource, Node root, View.OnTouchListener mTouchListener) {
 		super(context, resource);
 		this.context = context;
+		this.mTouchListener = mTouchListener;
 		this.root = root;
 	}
 
@@ -32,7 +36,11 @@ public class List extends ArrayAdapter {
 		
 		LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View row = inflater.inflate(R.layout.list_item, parent, false);
-
+		
+        if (row != convertView) 
+        {
+        	row.setOnTouchListener(mTouchListener);
+        }
 		TextView textView = (TextView)row.findViewById(R.id.textView1);		
 		new DownloadLocalNode().execute(textView, root.local.get(position));
 		
