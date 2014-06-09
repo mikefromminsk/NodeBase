a98dc683237ece43eeffa26531dd02b8a3e83677
x29a100 <x29a100@b0f4f0ed-4edc-434a-b90a-db1df2d90469>
2014-06-09 09:01:26 +0000

    git-svn-id: svn://100.93.93.31/meta@417 b0f4f0ed-4edc-434a-b90a-db1df2d90469

--- src/G/Console.dfm
+++ src/G/Console.dfm
@@ -1,8 +1,8 @@
 object Form1: TForm1
   Left = 193
   Top = 124
-  Width = 928
-  Height = 480
+  Width = 348
+  Height = 205
   Caption = 'Form1'
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
--- src/G/GModule.pas
+++ src/G/GModule.pas
@@ -3,23 +3,96 @@ unit GModule;
 interface
 
 uses
-  MetaBaseModule, Dialogs;
+  MetaBaseModule, Dialogs, Types, Math;
 
 type
   TG = class (TFocus)
   public
     function Exec(Line: String): PNode; override;
-    //procedure CreateVariables(Node: PNode);
+    function CreateNode(Node: PNode): PNode;
+    function AddNode(var Arr: ANode; Node: PNode): PNode;
+    procedure CreateModule(Node: PNode);
+    procedure CreateObjects(Node: PNode);
+    procedure CreateCopys(Node: PNode);
+    procedure CreateData(Node: PNode);
+    procedure CreateTypes(Node: PNode);
+    procedure CreateFunctionHead(Node: PNode);
+    procedure CreateParameters(Node: PNode);
+    procedure CreateResult(Node: PNode);
+    procedure CreateSequence(Node: PNode);
+    procedure CreateValue(Node: PNode);
+    procedure SetParameters(Node: PNode);
+    procedure CreateIf(Node: PNode);
+    procedure CreateFor(Node: PNode);
   end;
+
+const
+  LocalCount = 10;
+  FunctionCount = 3;
 
 implementation
 
 
+
+function Random(Range: Integer): Integer;
+begin
+  Result := Range;
+end;
+
+function TG.AddNode(var Arr: ANode; Node: PNode): PNode;
+begin
+  Result := AddSubNode(Arr);
+  Arr[High(Arr)] := Node;
+end;
+
+function RandomIndex(Arr: TIntegerDynArray): Integer;
+var i, Index: Integer;
+begin
+  Index := Random(MaxInt) mod SumInt(Arr);
+  for i:=0 to High(Arr) do
+  begin
+    if Index - Arr[i] < 0 then
+    begin
+      Result := i;
+      Exit;
+    end;
+    Index := Index - Arr[i];
+  end;
+end;
+
+procedure TG.CreateObjects(Node: PNode);
+var i: Integer;
+begin
+  for i:=0 to LocalCount do
+  begin
+    LocalNode := NewNode(NextId);
+    AddLocal(Node, LocalNode);
+  end;
+end;
+
+procedure TG.CreateParameters(Node: PNode);
+begin
+
+end;
+
+procedure TG.CreateFunctionHead(Node: PNode);
+var i: Integer;
+begin
+  for i:=0 to FunctionCount do
+  begin
+    CreateParameters(Node);
+  end;
+end;
+
+function TG.CreateModule(Node: PNode): PNode;
+begin
+  CreateLocal(Result);
+  Result := Node;
+end;
+
 function TG.Exec(Line: String): PNode;
 begin
-  Result := inherited Exec(Line);
-  //if Result.Generator
-  ShowMessage(GetNodeBody(Result));
+  Result := CreateModule(inherited Exec(Line));
 end;
 
 end.
