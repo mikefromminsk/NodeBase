999fa11df403601ddbf4840f73c74635304b0e82
x29a100 <x29a100@mail.ru>
2014-12-10 19:44:53 +0300

    java reflection

--- src/Console.java
+++ src/Console.java
@@ -1,4 +1,65 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class Console {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+public class Console extends JFrame {
+
+	public static void main(String[] args) {
+		ModuleLoader loader = new ModuleLoader("C:\\Users\\Hist\\Desktop\\ClassLoader\\src\\", ClassLoader.getSystemClassLoader());
+		
+	      try {
+	        
+	        Class c = loader.loadClass("Module");
+	        Method[] methods = c.getMethods(); 
+	        for (Method method : methods) { 
+	            System.out.println("Имя: " + method.getName()); 
+	            System.out.println("Возвращаемый тип: " + method.getReturnType().getName()); 
+	         
+	            Class[] paramTypes = method.getParameterTypes(); 
+	            System.out.print("Типы параметров: "); 
+	            for (Class paramType : paramTypes) { 
+	                System.out.print(" " + paramType.getName()); 
+	            } 
+	            System.out.println(); 
+	        }
+	        
+	        Object obj = c.newInstance();
+	        Class[] paramTypes = new Class[] { int.class }; 
+	        Method method = c.getMethod("run", paramTypes); 
+	        Object[] params = new Object[] { new Integer(10) }; 
+	        Object ret = method.invoke(obj, params);
+	        System.out.print("вернулось " + ((Integer)ret).toString()); 
+	        
+	      } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	      } catch (InstantiationException e) {
+	        e.printStackTrace();
+	      } catch (IllegalAccessException e) {
+	        e.printStackTrace();
+	      } catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	   
+	}
+
 
 }
